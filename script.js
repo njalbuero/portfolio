df6a17b9202e20828655b0f347c467a0dd95f09b
Nhielsan <njalbuero@gmail.com>
2021-07-10 18:05:11 +0800

    change style

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const toggleBtn = document.querySelector(".toggle-button");
 const navbarLinks = document.querySelector(".navbar-links");
 const links = document.querySelectorAll(".navLink");
-//for navbar
+const btn_view = document.querySelector(".about-content-container a");
+
+//set about nav link color to active at start
+links[0].classList.add("primary-color");
+
+//navbar toggle
 toggleBtn.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active");
+  navbarLinks.classList.toggle("active");
 });
 
-links.forEach(element => {
-    element.addEventListener("click", () => {
+links.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (navbarLinks.classList.contains("active")) {
+      navbarLinks.classList.toggle("active");
+    }
+    links.forEach((link) => {
+      link.classList.remove("primary-color");
+    });
+    element.classList.add("primary-color");
+  });
+});
 
-        if (navbarLinks.classList.contains("active")) {
-            navbarLinks.classList.toggle("active");
-        }
-    })
+//view projects button - set projects nav link to active
+btn_view.addEventListener("click", () => {
+  links.forEach((link) => {
+    link.classList.remove("primary-color");
+  });
+  links[1].classList.add("primary-color");
 });
