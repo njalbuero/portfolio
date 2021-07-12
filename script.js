15349c15b2ec7f7866be2686fff2aca471473fa9
Nhielsan <njalbuero@gmail.com>
2021-07-12 15:43:02 +0800

    finish new style

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const links = document.querySelectorAll(".navLink");
 const btn_view = document.querySelector(".about-content-container a");
 
 //set about nav link color to active at start
-links[0].classList.add("primary-color");
+// links[0].classList.add("primary-color");
 
 //navbar toggle
 toggleBtn.addEventListener("click", () => {
@@ -16,17 +16,31 @@ links.forEach((element) => {
     if (navbarLinks.classList.contains("active")) {
       navbarLinks.classList.toggle("active");
     }
-    links.forEach((link) => {
-      link.classList.remove("primary-color");
-    });
-    element.classList.add("primary-color");
+    // links.forEach((link) => {
+    //   link.classList.remove("primary-color");
+    // });
+    // element.classList.add("primary-color");
   });
 });
 
 //view projects button - set projects nav link to active
-btn_view.addEventListener("click", () => {
-  links.forEach((link) => {
-    link.classList.remove("primary-color");
-  });
-  links[1].classList.add("primary-color");
-});
+// btn_view.addEventListener("click", () => {
+//   links.forEach((link) => {
+//     link.classList.remove("primary-color");
+//   });
+//   links[1].classList.add("primary-color");
+// });
+
+const sections = document.querySelectorAll(".section");
+var observer = new IntersectionObserver(
+  function () {
+    // isIntersecting is true when element and viewport are overlapping
+    // isIntersecting is false when element and viewport don't overlap
+    console.log("Element has just become visible in screen");
+  },
+  { threshold: [0] }
+);
+
+observer.observe(sections[0]);
+observer.observe(sections[1]);
+observer.observe(sections[2]);
